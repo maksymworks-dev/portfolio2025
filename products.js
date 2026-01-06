6f603095339dfbcc143c4d724a87181722972cce
Maksym Sobeshchuk <mr.max@Maksyms-MacBook-Air.local>
2026-01-06 12:32:37 +0100

    Refactor product data to use short links and products.js

---
+++ products.js
@@ -0,0 +1,45 @@
+const products = {
+    "dyakyu-royal-blue": {
+        name: "ДЯКУЮ, ROYAL BLUE",
+        image: "projects/dyakyu-royal-blue.jpg",
+        additionalImages: "projects/IMG_0966.JPG|projects/IMG_0578.JPG|projects/IMG_5911.JPG|projects/IMG_6204.JPG|projects/IMG_6338.JPG|projects/IMG_0577.JPG|projects/IMG_6211.JPG",
+        priceId: ""
+    },
+    "polska-black": {
+        name: "POLSKA, BLACK",
+        image: "projects/IMG_2258.jpeg",
+        additionalImages: "projects/IMG_1486.JPG|projects/IMG_1462.JPG",
+        priceId: ""
+    },
+    "wariat-black": {
+        name: "WARIAT, BLACK",
+        image: "projects/IMG_6665.JPG",
+        additionalImages: "projects/IMG_6620.JPG|projects/IMG_6619.JPG",
+        priceId: ""
+    },
+    "ich-bin-aus-saporischschja": {
+        name: "Ich bin aus Saporischschja",
+        image: "projects/IMG_4650.JPG",
+        priceId: ""
+    },
+    "warsaw-guide": {
+        name: "WARSAW GUIDE",
+        image: "projects/IMG_3944.JPG",
+        additionalImages: "projects/3236496D-13D0-489D-9227-E0CAB0905511.jpg|projects/wg front.jpg",
+        priceId: ""
+    },
+    "star-cowboy": {
+        name: "STAR COWBOY UPCYCLED JACKET",
+        image: "projects/scowboy3.jpg",
+        additionalImages: "projects/scowboy1.jpg|projects/scowboy2.jpg",
+        description: "Upcycled jacket with cowboy embroidery. Made in collaboration with <a href='https://ihatemusic.online' target='_blank'>ihatemusic</a>.",
+        buyLink: "https://ihatemusic.online/products/star-cowboy-upcycled-jacket",
+        priceId: ""
+    },
+    "swaga-cup": {
+        name: "СВАГА CUP X SKOE",
+        image: "projects/IMG_5678.JPG",
+        additionalImages: "projects/IMG_5672.JPG",
+        priceId: ""
+    }
+};
